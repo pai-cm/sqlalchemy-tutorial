8c7724552edf21dcd839911682fbaa44a2a49ccf
pai-cm <cmkim@publicai.co.kr>
2024-04-29 19:44:52 +0900

    커넥션 맺는 것이 더 오래 걸린다

--- tests/test_connect_to_db.py
+++ tests/test_connect_to_db.py
@@ -4,6 +4,8 @@
 import logging
 import time
 
+import sqlalchemy
+
 
 async def test_measure_elapsed_time_per_step(test_engine):
     start_time = time.perf_counter()
@@ -22,3 +24,18 @@ async def test_measure_elapsed_time_per_step(test_engine):
         """
         elapsed_time = time.perf_counter() - start_time
         logging.info(f"(2) 두번째 데이터베이스 연결에 걸린 시간: {elapsed_time:.4f}s")
+
+
+async def test_measure_elapsed_time_per_step2(test_engine):
+    start_time = time.perf_counter()
+    logging.info("쿼리 시작하기 >>>")
+    async with test_engine.connect() as conn:
+        """이게 커넥션 맺은 것음"""
+        elapsed_time = time.perf_counter() - start_time
+        logging.info(f"(1) 데이터베이스 연결에 걸린 시간: {elapsed_time:.4f}s")
+
+        """커넥션을 맺는 것이 오래 걸린다라는 것을 보여줌"""
+        start_time = time.perf_counter()
+        await conn.execute(sqlalchemy.text("select 1;"))
+        elapsed_time = time.perf_counter() - start_time
+        logging.info(f"(2) select query 날리는데 걸린 시간: {elapsed_time:.4f}s")
