3be0f4db417bcef82f206576bb9ee16d5066bbb2
pai-cm <cmkim@publicai.co.kr>
2024-04-29 19:57:15 +0900

    pool recycle

--- tests/test_connect_to_db.py
+++ tests/test_connect_to_db.py
@@ -5,6 +5,7 @@ import logging
 import time
 
 import sqlalchemy
+from sqlalchemy.ext.asyncio import create_async_engine
 
 
 async def test_measure_elapsed_time_per_step(test_engine):
@@ -29,6 +30,48 @@ async def test_measure_elapsed_time_per_step(test_engine):
 async def test_measure_elapsed_time_per_step2(test_engine):
     start_time = time.perf_counter()
     logging.info("쿼리 시작하기 >>>")
+    async with test_engine.connect() as conn:
+        """이게 커넥션 맺은 것음"""
+        elapsed_time = time.perf_counter() - start_time
+        logging.info(f"(1) 데이터베이스 연결에 걸린 시간: {elapsed_time:.4f}s")
+
+        """커넥션을 맺는 것이 오래 걸린다라는 것을 보여줌"""
+        start_time = time.perf_counter()
+        await conn.execute(sqlalchemy.text("select 1;"))
+        elapsed_time = time.perf_counter() - start_time
+        logging.info(f"(2) select query 날리는데 걸린 시간: {elapsed_time:.4f}s")
+
+
+async def test_pool_recycle(test_engine):
+    test_engine = create_async_engine(
+        'postgresql+asyncpg://user:password@localhost:5434/testdb',
+        echo=True,
+        pool_recycle=1,  # 오래된 거 안 먹어, 안써, 풀의 오래된 커넥션은 안쓸거야 라고 설정하는 것 (안쓴지 한시간 이라는 개념)
+        pool_pre_ping=True,  # 매번 커넥션을 쓸 때마다 select 1 을 쓰기 때문에 헤비 트래픽일 때는 이것도 성능에 문제가 된다
+    )
+
+    start_time = time.perf_counter()
+    async with test_engine.connect() as conn:
+        """이게 커넥션 맺은 것음"""
+        elapsed_time = time.perf_counter() - start_time
+        logging.info(f"(1) 데이터베이스 연결에 걸린 시간: {elapsed_time:.4f}s")
+
+        """커넥션을 맺는 것이 오래 걸린다라는 것을 보여줌"""
+        start_time = time.perf_counter()
+        await conn.execute(sqlalchemy.text("select 1;"))
+        elapsed_time = time.perf_counter() - start_time
+        logging.info(f"(2) select query 날리는데 걸린 시간: {elapsed_time:.4f}s")
+
+    time.sleep(2)
+    """
+    being returned to pool
+    reset, transaction already reset
+    exceeded timeout; recycling
+    Hard-closing connection
+    """
+    """새로운 커넥션을 맺을거니까, 아마 시간이 오래 걸려서 다른"""
+
+    start_time = time.perf_counter()
     async with test_engine.connect() as conn:
         """이게 커넥션 맺은 것음"""
         elapsed_time = time.perf_counter() - start_time
